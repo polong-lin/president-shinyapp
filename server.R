620f83c2806a6101df50c52af699583439f777d1
polong-lin <polong.lin+github@gmail.com>
2014-06-23 03:43:59 -0400

    for peer assignment

--- server.R
+++ server.R
@@ -1,17 +1,21 @@
 library(shiny)
 shinyServer(
     function(input, output) {
+        ## Various output variables
         output$overifiedText <- renderPrint("Presidential Status: Unverified")
         output$onameVar <- renderPrint({input$nameVar})
         output$ocountryVar <- renderPrint({input$countryVar})
         output$osecretVar <- renderPrint({input$secretVar})
         output$owelcome <- renderPrint({"Verification Required"})
+        ## Part of the verfication process that toggles the conditional panels in the ui.R
         output$overificationSuccess <- renderText({
             if (input$verifyButton > 0 & nchar(input$nameVar) > 0 & nchar(input$countryVar) > 0 & (as.numeric(input$secretVar2)^2 +1) == input$secretVar1 & input$checkbox == TRUE)
                 "STATUS: VERIFIED"
             else
                 "STATUS: UNVERIFIED"
             })
+        
+        ## Near the VERIFY button: text that tells you that you are unverified, or that you need to double-check your credentials
         output$overifiedText <- renderText({
             if (input$verifyButton == 0) 
                 paste("Presidential Status for: \"", input$nameVar, "\" of \"", input$countryVar, "\"", sep = "")
@@ -24,10 +28,13 @@ shinyServer(
             if (nchar(input$nameVar) > 0 & nchar(input$countryVar) > 0 & (as.numeric(input$secretVar2)^2 +1) == input$secretVar1 & input$checkbox == TRUE)
                 paste("Welcome back, President ", input$nameVar, " of ", input$countryVar)
             })
+        ## Output text for coordinates at missle launch
         output$olaunchOutput <- renderText(
             if (input$launchButton %% 2 == 1)
                 paste("Report: Launch success at coordinates: (", input$plotX, ", ", input$plotY, ")", sep ="")
         )
+        
+        ## Plot for the planets and the moon
         output$oscatter <- renderPlot({
             if(input$selectPlanet == "Venus")
                 planetColour = "lightyellow"
@@ -40,8 +47,10 @@ shinyServer(
                  ylab = "",
                  xlab = "",
                  main = paste("Satellite image of region of interest on", input$selectPlanet))
+            ## Horizontal and Vertical lines used for aiming
             abline(h = input$plotY, lwd = 3)
             abline(v = input$plotX, lwd = 3)
+            ## Supposed to create a permanent point on the graph - a bug to fix in the future.
             if (input$launchButton %% 2 == 1) 
                 plot(1:100, 1:100, xlim = c(0,100), ylim = c(0,100), pch = "",
                      ylab = "",
--- ui.R
+++ ui.R
@@ -2,27 +2,34 @@ library(shiny)
 shinyUI(pageWithSidebar(
     headerPanel("The President's Interplanetary-Missile-Launching Control Box"),
     mainPanel(
+        ## Conditional Panel that pops up after the credentials are "verified"
         conditionalPanel(
         condition = 'output.overificationSuccess == "STATUS: VERIFIED"',
         h4("Welcome back, President"),
         h2("Missile Launcher Controls"),
         h5(paste("Maximum Missile Range: Neighbouring Planets")),
+        ## Choosing planets
         selectInput("selectPlanet", label = h4("Select your Planet:"), 
                     choices = list("Venus" = "Venus", "Moon" = "the Moon",
                                    "Mars" = "Mars"), selected = 1),
+        ##Slider bars
         sliderInput('plotX', 
                     'Control the X-axis', 
                     value = 50, min = 0, max = 100, step = 1),
         sliderInput('plotY', 
                     'Control the Y-axis', 
                     value = 50, min = 0, max = 100, step = 1),
+        
+        ##Launch button and some text output
         actionButton("launchButton", "LAUNCH"),
         verbatimTextOutput("olaunchOutput"),
+        ## Plot
         plotOutput("oscatter")
         )
     ),
     sidebarPanel(
         conditionalPanel(
+            ## Conditional panel that appears before credentials are verified
             condition = 'output.overificationSuccess == "STATUS: UNVERIFIED"',
             textInput('nameVar', 'Insert your name'),
             br(),
